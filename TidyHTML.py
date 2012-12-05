a848a662fc5e25ad4393c41713c4dbf302b29ec1
Warin <woozeu+github@gmail.com>
2012-12-05 19:31:02 +0100

    Added settings files
    Plugin now follow the options set in the TidyHTML.sublime-settings
    
    Options available: see http://w3c.github.com/tidy-html5/quickref.html

--- TidyHTML.py
+++ TidyHTML.py
@@ -4,18 +4,36 @@ import os
 
 class TidyHtmlCommand(sublime_plugin.TextCommand):
     def run(self, edit):
+
+
         #windows require full path. Where is the binary stored relative to /Packages
         if sublime.platform() == "windows":
             path = sublime.packages_path() + "\\TidyHTML\\bin\\tidy.exe"
         else:
             path = "tidy"
 
+        #defining command to run
+        cmdToRun = [path, "-imq"]
+
+        #reading settings file
+        settings = sublime.load_settings('TidyHTML.sublime-settings')
+        options = settings.get('TidyHTMLOptions')
+
+        #extending command to run in case settings are defined
+        if options!=None:
+            for option in options:
+                for key in option:
+                    cmdToRun.append("--"+ key)
+                    cmdToRun.append(option[key])
+
         #limited to html files
         if len(self.view.file_name()) > 0 and self.view.file_name().endswith((".html", ".htm")):
 
             folder_name, file_name = os.path.split(self.view.file_name())
+            cmdToRun.append(file_name)
+
             self.view.window().run_command('exec', {
-                'cmd': [path, "-imq", "-wrap", "0", file_name],
+                'cmd': cmdToRun,
                 'working_dir': folder_name
             })
             sublime.status_message(("Cleanup HTML in %s...") % file_name)
